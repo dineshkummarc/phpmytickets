d2ef138eb0e6b7e62efd9f280ad86e1edd14e34a
Dinesh Kumar Choudhary <mapmydestination@gmail.com>
2021-02-03 12:54:37 +0530

    update

--- include/psl-config.php
+++ include/psl-config.php
@@ -3,8 +3,8 @@
  * These are the database login details
  */  
 define("HOST", "localhost");     // The host you want to connect to.
-define("USER", "tdappuser");    // The database username. 
-define("PASSWORD", "^s1vSG,0o4il");    // The database password. 
+define("USER", "root");    // The database username. 
+define("PASSWORD", "");    // The database password.
 define("DATABASE", "ticket32145");    // The database name.
  
 define("CAN_REGISTER", "any");
