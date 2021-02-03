683a2e64c4a325bb1ab630ef2ca341627686244c
Dinesh Kumar Choudhary <mapmydestination@gmail.com>
2021-02-03 14:34:13 +0530

    update

--- include/navbar.php
+++ include/navbar.php
@@ -33,6 +33,7 @@ $systemBrand = system::withName('brand'); # get branding for nav bar
 
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
             <ul class="nav navbar-nav">
+			<li id="dashboard"><a href="main.php">Dashboard</a></li>
                 <li id="tickets" class="dropdown">
                     <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Tickets <span class="caret"></span></a>
                     <ul class="dropdown-menu ">
